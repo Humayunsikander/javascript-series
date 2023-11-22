bca85cbb98974ecfc5d0fb0bf0befef088a1a293
Humayun Sikander <humayunsikander@gmail.com>
2023-11-22 08:05:13 +0000

    Parctised chapter 1.

--- 01_basics/04_chapter1_practise_set.js
+++ 01_basics/04_chapter1_practise_set.js
@@ -3,3 +3,14 @@
 let stringVariable = "Humayun";
 let age = 5;
 console.log("My name and age is ", stringVariable + age);
+
+//Find type of calculated value 
+console.log(typeof(stringVariable + age));
+
+//Create an object and assign value for latter
+const customObject = {
+    name : "Humayun"
+}
+
+customObject.age =  27
+console.log(customObject)
