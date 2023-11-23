15914c01b7e2bdeef232fdbe7daaa23cbc6a5694
Humayun Sikander <humayunsikander@gmail.com>
2023-11-23 05:25:18 +0000

    Javascript operators covered

---
+++ 01_basics/05_javascript_operators.js
@@ -0,0 +1,45 @@
+console.log("Javascript operators");
+
+
+let a = 5;
+let b = 10;
+
+// Arethmatic operators
+console.log("Arethmatic operators");
+
+console.log("a + b = ", a + b)
+console.log("a - b = ", a - b)
+console.log("a * b = ", a * b)
+console.log("a / b = ", a / b)
+console.log("a % b = ", a % b)
+console.log("a ** b = ", a ** b)
+console.log("++a = ", ++a) // 1st adds and then print 
+console.log("a++ = ", a++) // 1st print and then adds
+console.log("--a = ", --a) // 1st Subtracts and then print
+console.log("a-- = ", a--) // 1st print and then subtracts
+
+//Assignment operators
+console.log("Assignment operators");
+
+console.log("a = ", a); // a = 5
+console.log("b = ", b); // b = 10
+console.log("a += b = ", a += b); // a = 15
+console.log("a -= b = ", a -= b); // a = 5
+console.log("a *= b = ", a *= b); // a = 50
+console.log("a /= b = ", a /= b); // a = 5
+console.log("a = ", a); // a = 5
+console.log("b = ", b); // b = 10
+console.log("a %= b = ", a %= b); // a = 5
+console.log("a **= b = ", a **= b); // a ^ b (a power b)
+
+// Comparison operator 
+console.log("Comparison operators");
+
+console.log("a == b ", a == b);
+console.log("a != b ", a != b);
+console.log("a <= b ", a <= b);
+console.log("a >= b ", a >= b);
+console.log("a < b ", a < b);
+console.log("a > b ", a > b);
+console.log("a === b ", a === b); // return true if types of operands matches
+console.log("a !== b ", a !== b); // return true if types of operands not matches
